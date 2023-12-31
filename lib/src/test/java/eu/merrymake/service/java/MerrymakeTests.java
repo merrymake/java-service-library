5c2581ed89c8bb1bfa27c3a68b7fd66b9ea7902e
Christian Clausen <maxclausen@gmail.com>
2023-12-31 15:50:41 +0100

    Fix tests

--- lib/src/test/java/eu/merrymake/service/java/MerrymakeTests.java
+++ lib/src/test/java/eu/merrymake/service/java/MerrymakeTests.java
@@ -5,44 +5,49 @@ import org.junit.jupiter.api.Test;
 
 public class MerrymakeTests {
 
-    enum Result { Nothing, A, B, Init ;}
+    enum Result {
+        Nothing, A, B, Init;
+    }
 
     @Test
     public void first_action() {
-        final var spy = new Result[]{Result.Nothing};
-        Merrymake.service(new String[]{"a", "{}"}).handle("a", (payload, envelope) -> {
-            spy[0] = Result.A;
-        }).handle("b", (payload, envelope) -> {
-            spy[0] = Result.B;
-        }).init(() -> {
-            spy[0] = Result.Init;
-        });
+        final var spy = new Result[] { Result.Nothing };
+        Merrymake.service(new String[] { "a", "{ \"messageId\": \"mid\", \"traceId\": \"tid\" }" })
+                .handle("a", (payload, envelope) -> {
+                    spy[0] = Result.A;
+                }).handle("b", (payload, envelope) -> {
+                    spy[0] = Result.B;
+                }).init(() -> {
+                    spy[0] = Result.Init;
+                });
         Assertions.assertEquals(Result.A, spy[0]);
     }
 
     @Test
     public void second_action() {
-        final var spy = new Result[]{Result.Nothing};
-        Merrymake.service(new String[]{"b", "{}"}).handle("a", (payload, envelope) -> {
-            spy[0] = Result.A;
-        }).handle("b", (payload, envelope) -> {
-            spy[0] = Result.B;
-        }).init(() -> {
-            spy[0] = Result.Init;
-        });
+        final var spy = new Result[] { Result.Nothing };
+        Merrymake.service(new String[] { "b", "{ \"messageId\": \"mid\", \"traceId\": \"tid\" }" })
+                .handle("a", (payload, envelope) -> {
+                    spy[0] = Result.A;
+                }).handle("b", (payload, envelope) -> {
+                    spy[0] = Result.B;
+                }).init(() -> {
+                    spy[0] = Result.Init;
+                });
         Assertions.assertEquals(Result.B, spy[0]);
     }
 
     @Test
     public void init() {
-        final var spy = new Result[]{Result.Nothing};
-        Merrymake.service(new String[]{"", "{}"}).handle("a", (payload, envelope) -> {
-            spy[0] = Result.A;
-        }).handle("b", (payload, envelope) -> {
-            spy[0] = Result.B;
-        }).init(() -> {
-            spy[0] = Result.Init;
-        });
+        final var spy = new Result[] { Result.Nothing };
+        Merrymake.service(new String[] { "", "{ \"messageId\": \"mid\", \"traceId\": \"tid\" }" })
+                .handle("a", (payload, envelope) -> {
+                    spy[0] = Result.A;
+                }).handle("b", (payload, envelope) -> {
+                    spy[0] = Result.B;
+                }).init(() -> {
+                    spy[0] = Result.Init;
+                });
         Assertions.assertEquals(Result.Init, spy[0]);
     }
 
